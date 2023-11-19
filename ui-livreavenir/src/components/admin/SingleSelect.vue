b1b04f209c59187420f2a9f1c3780c4200bd7d87
Raulet Olha <raulet.olha@gmail.com>
2023-11-19 22:31:02 +0100

    ui: try to add single select

---
+++ ui-livreavenir/src/components/admin/SingleSelect.vue
@@ -0,0 +1,23 @@
+<template>
+    [{{ options }}]
+    <div>
+        <Multiselect
+            v-model.trim="value"
+            :options="options"
+            @select="updateValue"
+        />
+    </div>
+</template>
+  
+  <script setup>
+import Multiselect from "@vueform/multiselect";
+defineProps({
+    options: [],
+    value: Number,
+});
+const value = null;
+const updateValue = (value) => {
+    // Emit an event to update the value in the parent component
+    emit("updateValue", value);
+};
+</script>
