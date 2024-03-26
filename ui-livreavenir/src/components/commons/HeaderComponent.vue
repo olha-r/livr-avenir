71aa3a203cc0e68acc6f7d807f9dfd9d7fae0913
Raulet Olha <raulet.olha@gmail.com>
2024-03-26 16:52:33 +0100

    ui: modify navbar

--- ui-livreavenir/src/components/commons/HeaderComponent.vue
+++ ui-livreavenir/src/components/commons/HeaderComponent.vue
@@ -1,91 +1,85 @@
 <script setup>
 import { storeToRefs } from "pinia";
+import { ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { useAuthStore } from "@/stores/auth-store";
+import { useCartStore } from "@/stores/cart-store";
+import CartPreview from "@/components/client/CartPreview.vue";
 
 const { t } = useI18n();
 const authStore = useAuthStore();
+const cartStore = useCartStore();
 const { isAdmin, isLoggedIn, userFullName } = storeToRefs(authStore);
 const logout = () => {
     authStore.logout();
 };
+const showCartSidebar = ref(false);
+const toggleCartSidebar = () => {
+    showCartSidebar.value = !showCartSidebar.value;
+};
+const closeCartSidebar = () => {
+    showCartSidebar.value = false;
+};
 </script>
 
 <template>
-    <header>
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <RouterLink class="navbar-brand" :to="{ name: 'home' }"
-                    ><img
-                        src="../../assets/images/logo/logo.png"
-                        alt="logo"
-                        style="height: 40px"
-                /></RouterLink>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavDropdown"
-                    aria-controls="navbarNavDropdown"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
+    <header
+        class="d-flex flex-wrap justify-content-around py-3 mb-4 border-bottom"
+    >
+        <RouterLink
+            class="d-flex align-items-center col-md-3 mb-2 mb-md-0"
+            :to="{ name: 'home' }"
+            ><img
+                src="../../assets/images/logo/logo.png"
+                alt="logo"
+                style="height: 35px"
+        /></RouterLink>
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <RouterLink class="nav-link px-2" :to="{ name: 'home' }">{{
+                    t("navbar.homePageItem")
+                }}</RouterLink>
+            </li>
+            <li class="nav-item" v-if="isLoggedIn">
+                <RouterLink class="nav-link px-2" :to="{ name: 'profile' }"
+                    >User profile</RouterLink
                 >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <RouterLink
-                                class="nav-link"
-                                :to="{ name: 'home' }"
-                                >{{ t("navbar.homePageItem") }}</RouterLink
-                            >
-                        </li>
-                        <li class="nav-item" v-if="isLoggedIn">
-                            <RouterLink
-                                class="nav-link"
-                                :to="{ name: 'profile' }"
-                                >User profile</RouterLink
-                            >
-                        </li>
-                        <li class="nav-item" v-if="isLoggedIn && isAdmin">
-                            <RouterLink
-                                class="nav-link"
-                                :to="{ name: 'admin-dashboard' }"
-                                >{{ t("navbar.adminItem") }}</RouterLink
-                            >
-                        </li>
-                    </ul>
-
-                    <div class="navbar-header navbar-right pull-right">
-                        <ul class="nav navbar-nav navbar-right pull-right">
-                            <li class="nav-item" v-if="!isLoggedIn">
-                                <RouterLink
-                                    class="nav-link"
-                                    :to="{ name: 'register' }"
-                                    >{{ t("navbar.signUp") }}</RouterLink
-                                >
-                            </li>
-                            <li class="nav-item" v-if="!isLoggedIn">
-                                <RouterLink
-                                    class="nav-link"
-                                    :to="{ name: 'login' }"
-                                    >{{ t("navbar.login") }}</RouterLink
-                                >
-                            </li>
-
-                            <!-- <li>
-                            <i class="bi bi-person-circle"></i>
-                        </li> -->
-                        </ul>
-                    </div>
+            </li>
+            <li class="nav-item" v-if="isLoggedIn && isAdmin">
+                <RouterLink
+                    class="nav-link px-2"
+                    :to="{ name: 'admin-dashboard' }"
+                    >{{ t("navbar.adminItem") }}</RouterLink
+                >
+            </li>
+            <li class="nav-item" v-if="!isLoggedIn">
+                <RouterLink
+                    class="btn btn-outline-primary"
+                    :to="{ name: 'register' }"
+                    >{{ t("navbar.signUp") }}</RouterLink
+                >
+            </li>
+            <li class="nav-item" v-if="!isLoggedIn">
+                <RouterLink
+                    class="btn btn-outline-primary"
+                    :to="{ name: 'login' }"
+                    >{{ t("navbar.login") }}</RouterLink
+                >
+            </li>
+            <li class="nav-item" v-else>
+                <span @click="toggleCartSidebar">
+                    <span class="logout pe-2">{{
+                        cartStore.items.length
+                    }}</span>
+                    <i class="bi bi-cart-fill fs-2 logout pe-4"></i>
+                </span>
+                <div :class="['cart-sidebar', { show: showCartSidebar }]">
+                    <CartPreview @close="closeCartSidebar" />
                 </div>
-                <div class="navbar-text">
-                    <a href="#" @click="logout" v-if="isLoggedIn">
-                        <i class="bi bi-box-arrow-right fs-2 logout me-4"></i>
-                    </a>
-                </div>
-            </div>
-        </nav>
+                <a href="#" @click="logout" v-if="isLoggedIn">
+                    <i class="bi bi-box-arrow-right fs-2 logout me-4"></i>
+                </a>
+            </li>
+        </ul>
     </header>
 </template>
