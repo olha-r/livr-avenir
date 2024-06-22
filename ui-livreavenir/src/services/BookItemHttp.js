5b309d5c21309e3ce2101ea3917cc138b44dfd70
Raulet Olha <raulet.olha@gmail.com>
2024-06-22 10:08:26 +0200

    ui: fix book-item endpoint call

--- ui-livreavenir/src/services/BookItemHttp.js
+++ ui-livreavenir/src/services/BookItemHttp.js
@@ -2,30 +2,30 @@ import { http } from './axios';
 
 export class BookItemHttp {
 	async add_new_item(payload) {
-		const url = `/book_items`;
+		const url = `/book-items`;
 		const response = await http.post(url, payload);
 		return response;
 	}
 
 	async get_items_by_book_id(book_id) {
-		const url = `/book_items/${book_id}`;
+		const url = `/book-items/${book_id}`;
 		const response = await http.get(url);
 		return response;
 	}
 
 	async get_items_by_user() {
-		const url = `/book_items/user`;
+		const url = `/book-items/user`;
 		const response = await http.get(url);
 		return response;
 	}
 
 	async delete_item(item_id) {
-		const url = `/book_items/${item_id}`;
+		const url = `/book-items/${item_id}`;
 		const response = await http.delete(url);
 		return response;
 	}
 	async update_item(item_id, payload) {
-		const url = `/book_items/${item_id}`;
+		const url = `/book-items/${item_id}`;
 		const response = await http.patch(url, payload);
 		return response;
 	}
