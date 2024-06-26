62e0adad6009d435f001581c33b647d678ebd563
Raulet Olha <raulet.olha@gmail.com>
2024-06-26 14:32:20 +0200

    refactor AddPublisherStore

--- ui-livreavenir/src/components/admin/AddPublisherModal.vue
+++ ui-livreavenir/src/components/admin/AddPublisherModal.vue
@@ -15,53 +15,52 @@ const props = defineProps({
 });
 
 const emit = defineEmits(['onAdd', 'onClose']);
-
 const publisherInputs = reactive({
 	name: ''
 });
-
 const publisherStore = usePublisherStore();
 const pageStore = usePageStore();
-
 const { t } = useI18n();
+
 const requiredMessage = `${t('admin.validationMessages.required')}`;
-const publisherRules = computed(() => {
-	return {
-		name: {
-			required: helpers.withMessage(requiredMessage, required),
-			minLength: helpers.withMessage(
-				`${t('client.validationMessages.minLengthDescription')}`,
-				minLength(1)
-			),
-			maxLength: helpers.withMessage(
-				`${t('client.validationMessages.maxLengthDescription')}`,
-				maxLength(150)
-			)
-		}
-	};
+const minLengthMessage = `${t(
+	'client.validationMessages.minLengthDescription'
+)}`;
+const maxLengthMessage = `${t(
+	'client.validationMessages.maxLengthDescription'
+)}`;
+
+const createValidationRules = (field) => ({
+	required: helpers.withMessage(requiredMessage, required),
+	minLength: helpers.withMessage(minLengthMessage, minLength(1)),
+	maxLength: helpers.withMessage(maxLengthMessage, maxLength(150))
 });
 
+const publisherRules = computed(() => ({
+	name: createValidationRules('name')
+}));
+
 const publisherValidation = useVuelidate(publisherRules, publisherInputs);
+
+const showAlert = (type, message, duration = 5000) => {
+	pageStore.alert.type = type;
+	pageStore.alert.message = message;
+	pageStore.alert.show = true;
+	setTimeout(() => {
+		pageStore.alert.show = false;
+	}, duration);
+};
+
 const addPublisher = async () => {
 	await publisherValidation.value.$validate();
 	if (!publisherValidation.value.$error) {
 		const resp = await publisherStore.add_new_publisher(publisherInputs);
 		if (resp.status === 204) {
 			emit('onAdd', true);
-			pageStore.alert.type = 'success';
-			pageStore.alert.message = t('admin.addPublisherForm.successMessage');
-			pageStore.alert.show = true;
-			setTimeout(() => {
-				pageStore.alert.show = false;
-			}, 5000);
+			showAlert('success', t('admin.addPublisherForm.successMessage'));
 			closeModal();
 		} else {
-			pageStore.alert.type = 'error';
-			pageStore.alert.message = t('client.addPublisherForm.errorMessage');
-			pageStore.alert.show = true;
-			setTimeout(() => {
-				pageStore.alert.show = false;
-			}, 3000);
+			showAlert('error', t('client.addPublisherForm.errorMessage'), 3000);
 		}
 	}
 };
