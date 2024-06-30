562722d870983b6235dd6027603c4f88b47a9a91
Raulet Olha <raulet.olha@gmail.com>
2024-06-30 15:43:37 +0200

    delete component

--- api-livr-avenir/src/main/java/co/simplon/livravenir/dtos/AuthorUpdate.java
+++
@@ -1,9 +0,0 @@
-package co.simplon.livravenir.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-public record AuthorUpdate(
-	@NotBlank @Size(min = 1, max = 150) String firstName,
-	@NotBlank @Size(min = 1, max = 150) String lastName) {
-}
